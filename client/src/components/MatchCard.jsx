848bc48ecc5045593d1b96a56e4a1b103c16f1dc
Thiago Candia <candiathiago123@gmail.com>
2026-05-12 05:10:29 -0300

    fix/10-distance-between-equipment

--- client/src/components/MatchCard.jsx
+++ client/src/components/MatchCard.jsx
@@ -1,12 +1,14 @@
 export default function MatchCard({ match }) {
   return (
-    <div className="p-4 bg-white/5 rounded-xl border border-white/10">
+    <div className="bg-white/5 border border-white/10 rounded-xl p-6">
       <p>{new Date(match.fixture.date).toLocaleDateString()}</p>
 
-      <div className="grid grid-cols-3 items-center">
-        <div className="flex items-center gap-2 justify-start">
-          <img src={match.teams.home.logo} className="w-6 h-6" alt="local" />
-          <span>{match.teams.home.name}</span>
+      <div className="grid grid-cols-3 items-center text-center">
+        <div className="flex flex-col items-center gap-3 min-w-0">
+          <img src={match.teams.home.logo} className="w-16 h-16" alt="local" />
+          <h3 className="font-bold uppercase text-sm md:text-base truncate max-w-full">
+            {match.teams.home.name}
+          </h3>
         </div>
 
         <div className="text-center">
@@ -15,15 +17,18 @@ export default function MatchCard({ match }) {
           </span>
         </div>
 
-
-        <div className="flex items-center gap-2 justify-end">
-          <span>{match.teams.away.name}</span>
-          <img src={match.teams.away.logo} className="w-6 h-6" alt="visitante" />
+      <div className="flex flex-col items-center gap-3 min-w-0">
+          <img src={match.teams.away.logo} className="w-16 h-16" alt="visitante" />
+          <h3 className="font-bold uppercase text-sm md:text-base truncate max-w-full">
+            {match.teams.away.name}
+          </h3>
         </div>
       </div>
 
-      <p>Árbitro: {match.fixture.referee}</p>
-      <p>Estadio: {match.fixture.venue.name}</p>
+      <div className="mt-6 text-sm text-gray-400 space-y-1">
+        <p>Árbitro: {match.fixture.referee || "No informado"}</p>
+        <p>Estadio: {match.fixture.venue.name || "No informado"}</p>
+      </div>
     </div>
   )
 }
